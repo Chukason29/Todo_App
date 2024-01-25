e505284d067e4e7823aedf22538538c4171c48cf
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-01-25 09:18:25 +0100

    added createTask Function

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ const addTaskDiv = document.getElementById("add-task");
 const taskForm = document.getElementById("task-form")
 const cancel = document.getElementById("x")
 
+const title = document.getElementById("title");
+const date = document.getElementById("date")
+const description = document.getElementById("description")
+
+const tasks = []
+
 
 const hideElement = (toHide, toShow) => {
     toHide.classList.add("hide");
     toShow.classList.remove("hide")
+}
+const createTask = () =>{
+    const newTask = {}
+    newTask.taskId = tasks.length
+    newTask.taskTitle = title.value
+    newTask.taskDate = date.value
+    newTask.taskDescription = description.value
+    tasks.unshift(newTask);
 }
 addNewTask.addEventListener("click", () => {
     hideElement(addTaskDiv, taskForm)
@@ -15,3 +29,4 @@ addNewTask.addEventListener("click", () => {
 cancel.addEventListener("click", () =>{
     hideElement(taskForm, addTaskDiv)
 })
+createTask()
